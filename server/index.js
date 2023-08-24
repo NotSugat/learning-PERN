4a9396cf0d09e4fc8604caa9fa78dd0b4b249303
NotSugat <sugatsujakhu@gmail.com>
2023-08-24 22:56:04 +0545

    CRUD operation done

--- server/index.js
+++ server/index.js
@@ -12,6 +12,57 @@ app.use(express.json())
 //rest api
 
 // get all items
+app.get("/todos", async (req, res) => {
+	try {
+		const allTodos = await client.query("SELECT * FROM todo");
+		res.json(allTodos.rows)
+	} catch (error) {
+		console.log(error.message)
+
+	}
+})
+
+//get single item
+app.get("/todos/:id", async (req, res) => {
+	try {
+		const { id } = req.params
+
+		const todo = await client.query("SELECT * FROM todo WHERE todo_id = $1", [id])
+		res.json(todo.rows)
+		console.log(req.params)
+	} catch (error) {
+		console.log(error.message)
+
+	}
+})
+
+// update todo
+app.put("/todos/:id", async (req, res) => {
+	try {
+
+		const { id } = req.params
+		const { description } = req.body
+		const updatedTodo = await client.query("UPDATE todo SET description = $1 WHERE todo_id = $2 RETURNING *", [description, id])
+		res.json(updatedTodo.rows)
+	} catch (error) {
+		console.log(error.message)
+	}
+})
+
+
+// delete todo
+
+app.delete("/todos/:id", async (req, res) => {
+	try {
+		const { id } = req.params
+		const deletedTask = await client.query("DELETE FROM todo WHERE todo_id = $1 RETURNING *", [id])
+		res.json(deletedTask.rows)
+
+	} catch (error) {
+		console.log(error)
+	}
+})
+
 app.post("/todos", async (req, res) => {
 	try {
 		const { description } = req.body
@@ -23,6 +74,9 @@ app.post("/todos", async (req, res) => {
 
 	}
 })
+
+
+
 
 app.listen(5000, () => {
 	console.log("server listening in port 5000");
